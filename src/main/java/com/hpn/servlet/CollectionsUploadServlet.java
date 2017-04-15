e3deebd9e6a9cedd57382df64b5b47e8297ac008
603e <603e@sina.com>
2017-04-15 15:46:25 +0800

    删除注解

--- src/main/java/com/hpn/servlet/CollectionsUploadServlet.java
+++ src/main/java/com/hpn/servlet/CollectionsUploadServlet.java
@@ -7,7 +7,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
